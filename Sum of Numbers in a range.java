94f071bc24457904916d4a2376eca823f031d9f8
upendravirat18 <119523888+upendravirat18@users.noreply.github.com>
2023-02-17 20:10:32 +0530

    Create Sum of Numbers in a range.java
    Write a program to find the sum of numbers in a given range is discussed here. Given the starting and ending interval, the sum of all the numbers in that range will be displayed as output.
    
    For example,
    
    Input: 1 10
    
    Output: 55
    
    Explanation: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
    
    Input & Output Format:
    Input consists of two integers.
    
    First input consists of the start value.
    
    Second input consists of the end value.
    
    Output consists of the sum of range.
    
    Sample Input 1:
    
    1
    
    10
    
    Sample Output 1:
    
    55

---
+++ Sum of Numbers in a range.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Sumofnum
+{
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int c=0;
+      for(int i=a;i<=b;i++)
+      {
+          c=c+i;
+    
+      }System.out.print(c);
+    }
+}

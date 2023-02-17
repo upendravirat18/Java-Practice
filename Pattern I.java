7933b56db330772c62da67808ecc33a5ede8f125
upendravirat18 <119523888+upendravirat18@users.noreply.github.com>
2023-02-17 20:27:22 +0530

    Create Pattern I.java
    Write a Python Program to solve the pattern in dynamic.
    
    Sample Input 1:
    5
    Sample Output 1:
    *****
    *****
    *****
    *****
    *****
    Sample Input 2:
    7
    Sample Output 2:
    *******
    *******
    *******
    *******
    *******
    *******
    *******

---
+++ Pattern I.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Pattern1
+{
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int i,j;
+      for(i=1;i<=n;i++)
+      {
+          for(j=1;j<=n;j++)
+          {
+          System.out.print("*");
+          }System.out.println("");
+      }
+      
+    }
+}

4b7df0d22054fbc9e1a6d249f6e4120ac35f8dec
upendravirat18 <119523888+upendravirat18@users.noreply.github.com>
2023-02-17 20:28:55 +0530

    Create Pattern II.java
    Write a Python program to print the given pattern from user-defined values(dynamic values)
    
    Sample Input 1:
    5
    Sample Output 1:
    11111
    22222
    33333
    44444
    55555
    
    Sample Input 2:
    7
    Sample Output 2:
    1111111
    2222222
    3333333
    4444444
    5555555
    6666666
    7777777

---
+++ Pattern II.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Pattern2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                System.out.print(i);
+            }
+           System.out.println(""); 
+        }
+    
+    }
+}

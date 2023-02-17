8136f6d6c7a1c748f8d73fc0f4a414c13ce7b4f9
upendravirat18 <119523888+upendravirat18@users.noreply.github.com>
2023-02-17 20:12:28 +0530

    Create Series I.java
    Write a program to generate the following series --- 1,4,9,16,25, .... 
    
    Input format: 
    
    The input containing an integer which denotes 'n' 
    
    Output format: 
    
    Print the series and refer the sample output for formatting
    
    Sample Input :
    
    7
    
    Sample Output:
    
    1 4 9 16 25 36 49

---
+++ Series I.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Series1
+{
+    public static void main(String[] args)
+    {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     int a;
+     for(int i=1;i<=n;i++)
+     {
+         a=i*i;
+        System.out.printf("%d ",a);   
+       }
+     
+    }
+}

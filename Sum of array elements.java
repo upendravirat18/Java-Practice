29f8fab9690ab0b173dd1aaa9922475b38d0c19a
upendravirat18 <119523888+upendravirat18@users.noreply.github.com>
2023-02-17 20:46:46 +0530

    Create Sum of array elements.java
    Write a  program to find the sum of elements in an array.
    
    Sample Input:
    
    3
    1
    2
    3
    
    Sample Output:
    6

---
+++ Sum of array elements.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Sumarray
+{
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int a[]=new int[n];
+      int sum=0;
+      for(int i=0;i<n;i++)
+      {
+          
+          a[i]=sc.nextInt();
+          
+      }
+      for(int i=0;i<n;i++)
+      {
+      
+      sum=sum+a[i];
+      
+      }
+     System.out.println(sum);
+    }
+}                                 
